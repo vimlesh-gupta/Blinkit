cd865a5baad7fdeefd90ca147bb760f59f94c98a
Vimlesh Gupta <vimlesh.gupta089@gmail.com>
2025-07-11 18:57:37 +0530

    Initial Commit

--- Blinkit.py
+++ Blinkit.py
@@ -1,3 +1,6 @@
 import pandas as pd
+
+# 2. What is the average basket size (items per order)?
 df=pd.read_csv("blinkit_order_items.csv")
-print(df)
+average_basket_size=df.groupby('order_id')['quantity'].sum().mean()
+print(f"Average basket size: {average_basket_size:.2f} items per order")
